fa3508a7887641e2064d00cd7610dd53978c02e9
ajay baburaj <ajaybaburajp@gmail.com>
2023-06-24 13:39:58 +0530

    linkedlist

--- linkedlist/singly-linked-list/list.js
+++ linkedlist/singly-linked-list/list.js
@@ -1,8 +1,24 @@
-var head
+let head
 
 class Node{
     constructor(val){
-        this.val = val
+        this.data = val
         this.next = null
     }
+}
+
+function printList(){
+    let n= head;
+    while(n != null){
+        console.log(n.data)
+        n= n.next
+    }
+}
+
+
+function push(new_data){
+    let new_node = new Node(new_data)
+    new_node.next = head;
+    head = new_node
+
 }
